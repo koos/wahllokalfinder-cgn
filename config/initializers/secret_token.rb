d6dd234ddbc5a01ecd94a41eb92d424797cc68bd
Jan Kus <jan@railslove.com>
2013-08-22 15:31:46 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wahllokalfinder::Application.config.secret_key_base = '14272535a359611a2edf349e087cd34ffb10a5c3c9ef023392c1fcae6b16df4ec0760e73deb857bc4de07fe685a7c8d4ac266b07427d9011ee6e783e3d203781'
