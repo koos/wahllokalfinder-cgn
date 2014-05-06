b5266c6353ee9dcc32da6e797558de3f7b555281
Johannes Heck <johannes@endore.me>
2014-05-06 13:42:31 +0200

    redesign, js only

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,11 +1,7 @@
 class StationsController < ApplicationController
 
   def index
-    @without_wheelchair_stations = Station.where('latitude IS NOT NULL').where(barrier_free: false).where(vote_district_id: params[:number])
-    @with_wheelchair_stations = Station.where('latitude IS NOT NULL').where(barrier_free: true).where(vote_district_id: params[:number])
-    if (@without_wheelchair_stations.empty? && @with_wheelchair_stations.empty?) || params[:number]== ''
-      @without_wheelchair_stations = Station.where('latitude IS NOT NULL').where(barrier_free: false)
-      @with_wheelchair_stations = Station.where('latitude IS NOT NULL').where(barrier_free: true)
-    end
+    @without_wheelchair_stations = Station.where('latitude IS NOT NULL').where(barrier_free: false)
+    @with_wheelchair_stations = Station.where('latitude IS NOT NULL').where(barrier_free: true)
   end
 end
--- app/helpers/stations_helper.rb
+++ app/helpers/stations_helper.rb
@@ -1,2 +1,7 @@
 module StationsHelper
+
+  def stations?
+    @without_wheelchair_stations.length > 0 || @with_wheelchair_stations.length > 0
+  end
+
 end
