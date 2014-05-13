56cc5b866888739920894214885d30481dae82b0
Johannes Heck <johannes@endore.me>
2014-05-13 13:56:11 +0200

    some styling fixes, address-search

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,13 +3,21 @@ class Address < ActiveRecord::Base
   pg_search_scope :search_by_street, :against => :street, :using => { :tsearch => { :any_word => true } }
 
   def self.search(search_string)
-    zip = search_string.match(/\d{5}/)[0]
-    nr = search_string.match(/(\d+\w?)/)[0].to_i
-    if nr % 2 == 0
+    zip = search_string.match(/\d{5}/).to_a.try(:first)
+    nr = search_string.match(/(\d+\w?)/).to_a.try(:first).to_i
+    if zip && nr && nr % 2 == 0
       Address.where(zip: zip).search_by_street(search_string).where("number_even_from <= ? AND number_even_to >= ?", nr, nr)
-    else
+    elsif zip && nr
       Address.where(zip: zip).search_by_street(search_string).where("number_odd_from <= ? AND number_odd_to >= ?", nr, nr)
+    else
+      []
     end
+  end
+
+  def as_json(options={})
+    super(
+      only: :vote_district_id
+    )
   end
 
 end
