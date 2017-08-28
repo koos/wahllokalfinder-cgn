2cbcbb42ff8ba9d811dba3f18ef548bf6ecbf40f
Amed Kino <amed@xbyte.team>
2017-08-28 14:10:08 +0200

    Basic http authentications for rails admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -11,4 +11,10 @@ end
 
 RailsAdmin.config do |config|
   config.included_models = ["Station"]
+  config.authorize_with do
+    authenticate_or_request_with_http_basic('Login required') do |username, password|
+      username == Rails.application.secrets.admin_username &&
+      password == Rails.application.secrets.admin_password
+    end
+  end
 end
