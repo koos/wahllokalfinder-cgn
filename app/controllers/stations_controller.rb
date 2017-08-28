721973874ee180082d6030ecda6b030ccba674d4
Amed Kino <amed@xbyte.team>
2017-08-28 20:26:27 +0200

    Update readme and station ctrller

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,6 +1,15 @@
 class StationsController < ApplicationController
-
+  before_action :cities, only: [:city, :index]
+  layout 'city', only: [:city]
   def index
+    @cities = City.all
+  end
+
+  def search
+    render json: Address.search(params[:q]).first
+  end
+
+  def city
     @without_wheelchair_stations = Rails.cache.fetch('without_wheelchair_stations', expires_in: 5.hours) {
       Station.where('latitude IS NOT NULL').where(barrier_free: false)
     }
@@ -9,8 +18,9 @@ class StationsController < ApplicationController
     }
   end
 
-  def search
-    render json: Address.search(params[:q]).first
+  private
+  def cities
+    @cities = City.all
   end
 
 end
