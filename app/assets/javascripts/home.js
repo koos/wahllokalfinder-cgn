fb2cd4aeaf542902629fc6e72cee2b309532e660
Amed Kino <amed@xbyte.team>
2017-08-29 16:48:06 +0200

    Replace the map with Deutschland-noLabel map

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoieW95b3N0aWxlIiwiYSI6IktMYkRtYWMifQ.GR9h5n3DdQBUIUXVeh2-5A';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/yoyostile/cj6wd97gs09az2ro6ziz9d67y'
+    style: 'mapbox://styles/yoyostile/cj6xpembf1iab2ulh22v9h5vm'
 });
 
 geojson.features.forEach(function(marker) {
