be634b0b4bf10b41d35166eae7ca59f10b6c9c5f
Amed Kino <amed@xbyte.team>
2017-08-29 14:45:14 +0200

    Adding coordinates to City

--- db/migrate/20170828124244_create_cities.rb
+++ db/migrate/20170828124244_create_cities.rb
@@ -2,8 +2,10 @@ class CreateCities < ActiveRecord::Migration[5.1]
   def change
     create_table :cities do |t|
       t.string :name
+      t.string :slug
       t.text :zip
       t.string :state
+      t.string :coordinates
 
       t.timestamps
     end
--- db/migrate/20170828180826_add_slug_to_city.rb
+++
@@ -1,5 +0,0 @@
-class AddSlugToCity < ActiveRecord::Migration[5.1]
-  def change
-    add_column :cities, :slug, :string
-  end
-end
