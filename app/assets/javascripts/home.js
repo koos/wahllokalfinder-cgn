9d08a44456740a9c12b61c623604a1c4373a1a1b
Amed Kino <amed@xbyte.team>
2017-08-29 16:25:20 +0200

    Remove cities box from homePage and move js code to assets

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,20 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoieW95b3N0aWxlIiwiYSI6IktMYkRtYWMifQ.GR9h5n3DdQBUIUXVeh2-5A';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/yoyostile/cj6wd97gs09az2ro6ziz9d67y'
+});
+
+geojson.features.forEach(function(marker) {
+  // create lin
+  var aHerf = document.createElement('a')
+  aHerf.appendChild(document.createTextNode(marker.properties.title))
+  aHerf.href='/' + marker.properties.slug
+  var el = document.createElement('div');
+  el.appendChild(aHerf)
+  el.className = 'marker';
+
+  // make a marker for each feature and add to the map
+  new mapboxgl.Marker(el, { offset: [0, - 10] })
+  .setLngLat(marker.geometry.coordinates)
+  .addTo(map);
+});
