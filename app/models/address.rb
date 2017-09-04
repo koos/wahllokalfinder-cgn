f4851b7e8d123abc435da6b170f92ac0cc499687
Amed Kino <amed@xbyte.team>
2017-09-04 16:37:29 +0200

    Address PgSearch method change

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,31 +1,9 @@
 class Address < ActiveRecord::Base
   include PgSearch
-  pg_search_scope :search_by_street,
-                  against: :street,
-                  using: {
-                    tsearch: {
-                      any_word: true
-                    }
-                  }
-
-  # def self.search(search_string)
-  #   search_string = search_string.gsub('straße', 'str')
-  #   zip = search_string.match(/\d{5}/).to_a.try(:first)
-  #   nr = search_string.match(/(\d+\w?)/).to_a.try(:first).to_i
-  #   if nr != 0 && search_string[(search_string.index(nr.to_s).to_i - 1)] != " "
-  #     search_string.insert((search_string.index(nr.to_s)), " ")
-  #   end
-  #   return [] unless zip && nr
-  #   scope = Address.where(zip: zip).search_by_street(search_string)
-  #   if nr % 2 == 0
-  #     scope.where("number_even_from <= ? AND number_even_to >= ?", nr, nr)
-  #   else
-  #     scope.where("number_odd_from <= ? AND number_odd_to >= ?", nr, nr)
-  #   end
-  # end
+  pg_search_scope :search_by_street, against: :street
 
   def self.search_city(search_string, city)
-    search_string = search_string.gsub('straße', 'str')
+    search_string = search_string.gsub('straße', 'str').gsub(/[[:digit:]]{1,6}[[:alpha:]]/,'')
     nr = search_string.match(/(\d+\w?)/).to_a.try(:first).to_i
 
     if nr != 0 && search_string[(search_string.index(nr.to_s).to_i - 1)] != " "
@@ -33,9 +11,8 @@ class Address < ActiveRecord::Base
     end
 
     return [] unless city && nr
-  
-    scope = Address.where(zip: city.zip).search_by_street(search_string)
-    puts search_string
+    street_string = search_string.gsub(/[[:digit:]]/,'').gsub(/[[:digit:]]{5}/, '')
+    scope = Address.where(zip: city.zip).search_by_street(street_string)
     return [] if scope.count == 0
 
     case city.city_housing_type
