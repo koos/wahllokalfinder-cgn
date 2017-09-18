a058c1ef9d01f2065cec538b57dc0840144a8b65
Amed Kino <amed@xbyte.team>
2017-09-18 14:20:35 +0200

    Covering social-share-button facebook bug

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus email linkedin xing)
+  config.allow_sites = %w(twitter google_plus email linkedin xing)
 end
